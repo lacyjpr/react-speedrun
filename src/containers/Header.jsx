1579fb71c718d0db76b2272047f6b5eb37d7f810
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-07 12:31:02 -0700

    Cut Navbar content

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const Header = () => (
   <Navbar inverse collapseOnSelect fixedTop>
     <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#brand">React-Bootstrap</a>
-      </Navbar.Brand>
+      <Navbar.Brand>React-Speedrun</Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
-        </NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        <NavDropdown eventKey={1} title="Projects" id="basic-nav-dropdown">
+          <MenuItem eventKey={1.1}>Action</MenuItem>
+          <MenuItem eventKey={1.2}>Another action</MenuItem>
+          <MenuItem eventKey={1.3}>Something else here</MenuItem>
         </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">
-          Link Right
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link Right
-        </NavItem>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
