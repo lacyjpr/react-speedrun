26c69e75b3699a0893808514073c3a25b4a43277
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-08 08:29:49 -0700

    Clean out boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import { HashRouter, Link } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import Routes from './Routes';
 import Header from './containers/Header';
-
-import reactLogo from './assets/React-icon.png';
 
 const App = () => (
   <HashRouter>
     <main className="container">
       <Header />
-      <div>
-        <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working properly!</p>
-      </div>
-      <ul className="left">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/BackEnd/BookTradingClub">BookTradingClub</Link>
-        </li>
-      </ul>
-      <div>
-        <Routes />
-      </div>
+      <Routes />
     </main>
   </HashRouter>
 );
