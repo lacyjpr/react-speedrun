f828af125733dfa3bee11399fe9c5beb96c18c4a
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-08 04:14:23 -0700

    Add routing & Navlinks

--- src/containers/generate-projects.sh
+++ src/containers/generate-projects.sh
@@ -3,7 +3,7 @@
 # Uses a list of projects and generates a folder for
 # each, along with jsx, tests, scss and exports
 # necessary for a barebones functional component
-# Credit: @P1xt https://github.com/P1xt
+# Credit: @P1xt https://github.com/P1xt/speedrun-may2018-react/blob/master/client/utilities/generate-projects.sh
 ########################################################
 
 ########################################
@@ -120,3 +120,121 @@ for project in ${Backend_projects[@]}
 do
   createProject 'Backend' ${project}
 done
+
+########################################################
+# Everything below here is for creating the routing file
+########################################################
+
+# Create the Routing file
+cat << EOT >> ./ProjectRoutes.js
+import React from 'react';
+import { Route } from 'react-router-dom';
+EOT
+
+# Start constructing ProjectRoutes
+cat << EOT >> ./ProjectRoutes.js
+const ProjectRoutes = () => {
+  return (
+    <div>
+EOT
+
+
+# Add the routes to the routing file
+for project in ${Frontend_projects[@]}
+do
+  echo '      <Route path=\'/projects/frontend/${project}\' component={${project}} />' >> ./ProjectRoutes.js
+done
+for project in ${Datavis_projects[@]}
+do
+  echo '      <Route path=\'/projects/datavis/${project}\' component={${project}} />' >> ./ProjectRoutes.js
+done
+for project in ${Backend_projects[@]}
+do
+  echo '      <Route path=\'/projects/backend/${project}\' component={${project}} />' >> ./ProjectRoutes.js
+done
+
+# Close out the Routing file
+cat << EOT >> ./ProjectRoutes.js
+    </div>
+  );
+}
+export default ProjectRoutes;
+EOT
+
+#######################################################
+# Create dropdowns for navbar
+#######################################################
+# Create the NavRoutes file
+cat << EOT >> ./NavRoutes.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+const NavRoutes = () => {
+  return (
+    <Nav className='ml-auto' navbar>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Frontend
+        </DropdownToggle>
+        <DropdownMenu right>
+EOT
+for project in ${Frontend_projects[@]}
+do
+NAME=`echo ${project} | sed -e 's/\([A-Z]\)/ \1/g' -e 's/^ //'`
+cat << EOT >> ./NavRoutes.js
+          <DropdownItem>
+            <Link to='/projects/frontend/${project}'>${NAME}</Link>
+          </DropdownItem>
+EOT
+done
+cat << EOT >> ./NavRoutes.js
+        </DropdownMenu>
+      </UncontrolledDropdown>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Data Visualization
+        </DropdownToggle>
+        <DropdownMenu right>
+EOT
+for project in ${Datavis_projects[@]}
+do
+NAME=`echo ${project} | sed -e 's/\([A-Z]\)/ \1/g' -e 's/^ //'`
+
+cat << EOT >> ./NavRoutes.js
+          <DropdownItem>
+            <Link to='/projects/datavis/${project}'>${NAME}</Link>
+          </DropdownItem>
+EOT
+done
+cat << EOT >> ./NavRoutes.js
+        </DropdownMenu>
+      </UncontrolledDropdown>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Backend
+        </DropdownToggle>
+        <DropdownMenu right>
+EOT
+for project in ${Backend_projects[@]}
+do
+NAME=`echo ${project} | sed -e "s/\([A-Z]\)/ \1/g" -e "s/^ //"`
+cat << EOT >> ./NavRoutes.js
+          <DropdownItem>
+            <Link to='/projects/backend/${project}'>${NAME}</Link>
+          </DropdownItem>
+EOT
+done
+cat << EOT >> ./NavRoutes.js
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  );
+}
+export default NavRoutes;
+EOT
