7d17d6000ab04735f6e4de635fa1ae5652a04c2d
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-08 08:32:18 -0700

    Edit Home copy

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h2>Home Page</h2>
+    <h2>React Speedrun May 2018</h2>
+    <p>
+      Objective: Complete as many of the FCC projects as possible using React
+      during May 2018
+    </p>
   </div>
 );
 
