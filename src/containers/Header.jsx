666b2cdbd54cabf97216af8cd26c646008d9c962
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-08 08:01:06 -0700

    Add Frontend Projects to NavBar

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => (
   <Navbar inverse collapseOnSelect fixedTop>
@@ -10,10 +11,53 @@ const Header = () => (
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <NavDropdown eventKey={1} title="Projects" id="basic-nav-dropdown">
-          <MenuItem eventKey={1.1}>
-            <Link to="/FrontEnd/TributePage">Tribute Page</Link>
-          </MenuItem>
+        <NavDropdown eventKey={1} title="Front End" id="basic-nav-dropdown">
+          <LinkContainer to="/Frontend/TributePage">
+            <NavItem eventKey={1.1}>Tribute Page</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/PersonalPortfolioWebpage">
+            <NavItem eventKey={1.2}>Personal Portfolio</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/RandomQuoteMachine">
+            <NavItem eventKey={1.3}>Random Quote Machine</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/LocalWeather">
+            <NavItem eventKey={1.4}>Local Weather</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/WikipediaViewer">
+            <NavItem eventKey={1.5}>Wikipedia Viewer</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/Twitchtv">
+            <NavItem eventKey={1.6}>Twitch TV</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/Calculator">
+            <NavItem eventKey={1.7}>Calculator</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/PomodoroClock">
+            <NavItem eventKey={1.8}>Pomodoro Clock</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/TicTacToeGame">
+            <NavItem eventKey={1.9}>Tic Tac Toe</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/Frontend/SimonGame">
+            <NavItem eventKey={1.1}>Simon</NavItem>
+          </LinkContainer>
+        </NavDropdown>
+
+        <NavDropdown eventKey={1} title="Data Vis" id="basic-nav-dropdown">
+          <LinkContainer to="/Frontend/TributePage">
+            <NavItem eventKey={1.1}>Tribute Page</NavItem>
+          </LinkContainer>
+
+          <MenuItem eventKey={1.2}>Another action</MenuItem>
+          <MenuItem eventKey={1.3}>Something else here</MenuItem>
+        </NavDropdown>
+
+        <NavDropdown eventKey={1} title="Back End" id="basic-nav-dropdown">
+          <LinkContainer to="/Frontend/TributePage">
+            <NavItem eventKey={1.1}>Tribute Page</NavItem>
+          </LinkContainer>
+
           <MenuItem eventKey={1.2}>Another action</MenuItem>
           <MenuItem eventKey={1.3}>Something else here</MenuItem>
         </NavDropdown>
