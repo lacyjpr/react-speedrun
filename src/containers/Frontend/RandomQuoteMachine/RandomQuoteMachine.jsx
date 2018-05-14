204df7a62c937f9671ef67105d0b99edc634e8ba
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-14 09:38:13 -0700

    Style buttons

--- src/containers/Frontend/RandomQuoteMachine/RandomQuoteMachine.jsx
+++ src/containers/Frontend/RandomQuoteMachine/RandomQuoteMachine.jsx
@@ -13,18 +13,26 @@ class RandomQuoteMachine extends React.Component {
             id="quoteBox"
             className="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4"
           >
-            <p id="quote" className="text-center" />
-            <p id="author" className="text-right" />
+            <p id="quote" className="text-center">
+              random quote
+            </p>
+            <p id="author" className="text-right">
+              trump
+            </p>
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <button type="button" className="btn btn-secondary" id="tweet">
+                <button
+                  type="button"
+                  className="btn btn-primary pull-left"
+                  id="tweet"
+                >
                   Tweet
                 </button>
               </div>
               <div className="col-xs-6 col-sm-6 col-md-6>">
                 <button
                   type="button"
-                  className="btn btn-secondary pull-right"
+                  className="btn btn-primary pull-right"
                   id="newQuote"
                 >
                   New Quote
