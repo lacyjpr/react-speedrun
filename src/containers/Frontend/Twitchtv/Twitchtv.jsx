03747a3a9dddce0f90efa86bf5fa2ec1fb8138d7
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-18 08:20:37 -0700

    Style streamers

--- src/containers/Frontend/Twitchtv/Twitchtv.jsx
+++ src/containers/Frontend/Twitchtv/Twitchtv.jsx
@@ -50,53 +50,56 @@ class Twitchtv extends Component {
             <h1>Twitch.tv Streamers</h1>
           </div>
         </div>
-        <ul className="list-group">
-          {/* Credit @Kornil https://github.com/Kornil/react-speedrun/blob/master/src/containers/twitch_app/index.jsx & @P1xt https://github.com/P1xt/speedrun-may2018-p1xt/blob/master/fcc-react-client/src/Projects/Frontend/Twitchtv/Twitchtv.js */}
-          {/* {console.log(this.state.streamersData)} */}
-          {this.state.streamersData &&
-            //console.log(Object.entries(this.state.streamersData))
-            Object.entries(this.state.streamersData)
-              .sort((a, b) => (a[1].stream === null) - (b[1].stream === null))
-              .map(stream => {
-                // console.log(stream);
-                // if (stream[1].stream !== null) {
-                //   console.log(stream[1].stream.channel.logo);
-                // }
-                return (
-                  <li
-                    className="list-group-item Twitchtv__stream"
-                    key={stream[0]}
-                  >
-                    {stream[1].stream !== null ? (
-                      // (console.log(stream[1].stream.channel.logo),
-                      <img
-                        alt="avatar"
-                        src={stream[1].stream.channel.logo}
-                        width={50}
-                        height={50}
-                      />
-                    ) : (
-                      //)
-                      <img
-                        alt="missing avatar"
-                        src="http://via.placeholder.com/50x50"
-                      />
-                    )}
-                    <span className="Twitchtv__stream-name">{stream[0]}</span>
-                    {stream[1].stream &&
-                    stream[1].stream.channel.game !== null ? (
-                      <span>
-                        <a href={stream[1].stream.channel.url} target="_blank">
-                          &nbsp; {stream[1].stream.channel.status} &nbsp;
-                        </a>
+        {/* <ul className="list-group"> */}
+        {/* Credit @Kornil https://github.com/Kornil/react-speedrun/blob/master/src/containers/twitch_app/index.jsx & @P1xt https://github.com/P1xt/speedrun-may2018-p1xt/blob/master/fcc-react-client/src/Projects/Frontend/Twitchtv/Twitchtv.js */}
+        {/* {console.log(this.state.streamersData)} */}
+        {this.state.streamersData &&
+          //console.log(Object.entries(this.state.streamersData))
+          Object.entries(this.state.streamersData)
+            .sort((a, b) => (a[1].stream === null) - (b[1].stream === null))
+            .map(stream => {
+              // console.log(stream);
+              // if (stream[1].stream !== null) {
+              //   console.log(stream[1].stream.channel.logo);
+              // }
+              return (
+                <div
+                  className="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 Twitchtv__stream"
+                  key={stream[0]}
+                >
+                  {stream[1].stream !== null ? (
+                    // (console.log(stream[1].stream.channel.logo),
+                    <img
+                      alt="avatar"
+                      src={stream[1].stream.channel.logo}
+                      width={50}
+                      height={50}
+                    />
+                  ) : (
+                    //)
+                    <img
+                      alt="missing avatar"
+                      src="http://via.placeholder.com/50x50"
+                    />
+                  )}
+                  <span className="Twitchtv__stream-name">{stream[0]}</span>
+                  {stream[1].stream &&
+                  stream[1].stream.channel.game !== null ? (
+                    <span>
+                      <a href={stream[1].stream.channel.url} target="_blank">
+                        &nbsp; {stream[1].stream.channel.status} &nbsp;
+                      </a>
+                      <span className="Twitchtv__viewers">
+                        {stream[1].stream.viewers}
                       </span>
-                    ) : (
-                      <span> Offline </span>
-                    )}
-                  </li>
-                );
-              })}
-        </ul>
+                    </span>
+                  ) : (
+                    <span> Offline </span>
+                  )}
+                </div>
+              );
+            })}
+        {/* </ul> */}
       </div>
     );
   }
