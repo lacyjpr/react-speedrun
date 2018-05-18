5bc1d546201404cab8f8765234b82b4dcdcd62f7
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-18 06:56:33 -0700

    Buggy render

--- src/containers/Frontend/Twitchtv/Twitchtv.jsx
+++ src/containers/Frontend/Twitchtv/Twitchtv.jsx
@@ -67,16 +67,27 @@ class Twitchtv extends Component {
         </div>
         <ul className="list-group">
           {/* Credit @Kornil https://github.com/Kornil/react-speedrun/blob/master/src/containers/twitch_app/index.jsx & @P1xt https://github.com/P1xt/speedrun-may2018-p1xt/blob/master/fcc-react-client/src/Projects/Frontend/Twitchtv/Twitchtv.js */}
-          {console.log(this.state.streamersData)}
+          {/* {console.log(this.state.streamersData)} */}
           {this.state.streamersData &&
             //console.log(Object.entries(this.state.streamersData))
             Object.entries(this.state.streamersData)
               .sort((a, b) => (a[1].stream === null) - (b[1].stream === null))
               .map(stream => {
-                console.log(stream);
-                if (stream[1].stream !== null) {
-                  console.log(stream[1].stream.channel.logo);
-                }
+                // console.log(stream);
+                // if (stream[1].stream !== null) {
+                //   console.log(stream[1].stream.channel.logo);
+                // }
+                <li className="list-group-item" key={stream[1]}>
+                  {stream[1].stream !== null ? (
+                    (console.log(stream[1].stream.channel.logo),
+                    <img alt="avatar" src={stream[1].stream.channel.logo} />)
+                  ) : (
+                    <img
+                      alt="missing avatar"
+                      src="http://via.placeholder.com/50x50"
+                    />
+                  )}
+                </li>;
               })}
         </ul>
 
