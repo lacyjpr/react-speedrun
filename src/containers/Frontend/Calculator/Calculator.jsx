6d89753c598d37d9cb71b5bdbaa1476a50143dad
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-19 10:00:31 -0700

    Initialize state

--- src/containers/Frontend/Calculator/Calculator.jsx
+++ src/containers/Frontend/Calculator/Calculator.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './Calculator.scss';
 
 class Calculator extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      formula: [],
+      solution: '',
+    };
+  }
+
   handleClick = e => {
     console.log(e.target.value);
   };
