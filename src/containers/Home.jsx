1fda6f815fe7fba3ae2c34f555bde43c5f839a10
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-20 16:42:08 -0700

    Edit copy

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => (
   <div>
     <h2>React Speedrun May 2018</h2>
     <p>
-      Objective: Complete as many FCC projects as possible using React during
-      May 2018
+      Objective: Complete as many FCC projects as possible using React from May
+      9, 2018 to June 10, 2018
     </p>
     <a href="https://github.com/lacyjpr/react-speedrun" target="_blank">
       GitHub Repo
