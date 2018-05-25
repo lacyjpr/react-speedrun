a393939e29dc5bd6a295de78175fbebf5b2018a9
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-25 08:50:04 -0700

    Convert to class

--- src/containers/Frontend/TicTacToeGame/TicTacToeGame.jsx
+++ src/containers/Frontend/TicTacToeGame/TicTacToeGame.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TicTacToeGame.scss';
 
-const TicTacToeGame = () => (
-    <div className='TicTacToeGame'>
-      <h1>TicTacToeGame</h1>
-    </div>
-);
-
+class TicTacToeGame extends Component {
+  render() {
+    return (
+      <div className="TicTacToeGame">
+        <h1>TicTacToeGame</h1>
+      </div>
+    );
+  }
+}
 
 export default TicTacToeGame;
