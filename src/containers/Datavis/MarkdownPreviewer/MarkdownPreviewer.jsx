4ad068542a2b1fde111353e6bc2b21fc37414316
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-05 09:27:52 -0700

    Add container component

--- src/containers/Datavis/MarkdownPreviewer/MarkdownPreviewer.jsx
+++ src/containers/Datavis/MarkdownPreviewer/MarkdownPreviewer.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import marked from 'marked';
+
+import Input from './Input';
+import Output from './Output';
+
 import './MarkdownPreviewer.scss';
 
-const MarkdownPreviewer = () => (
-    <div className='MarkdownPreviewer'>
-      <h1>MarkdownPreviewer</h1>
-    </div>
-);
+class MarkdownPreviewer extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      text:
+        'Markdown Previewer\n==================\n\n# Heading 1\n## Heading 2 \n### Heading 3\n#### Heading 4\n##### Heading 5\n\nParagraphs are separated by a blank line.\n\nLeave 2 spaces at the end of a line to make a line break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nUnordered list:\n\n  * apples\n  * oranges\n  * pears\n\nOrdered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n\n *[Wikipedia](https://www.wikipedia.org/)*',
+    };
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  getMarkdown = () => {
+    var markdown = marked(this.state.text, { sanitize: true });
+    return {
+      __html: markdown,
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <Input onChange={this.handleChange} value={this.state.text} />
+        <Output markDown={this.getMarkdown()} />
+      </div>
+    );
+  }
+}
 
 export default MarkdownPreviewer;
